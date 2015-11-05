8feb992882bfef75d0287ffad704cd9b588cf139
huahua <huahua@Xuans-Mac-mini.local>
2015-11-05 16:27:52 +0800

    对block的用法进行了解释

--- app/controllers/interface/users_controller.rb
+++ app/controllers/interface/users_controller.rb
@@ -15,6 +15,8 @@ class Interface::UsersController < ActionController::Base
     }
   end
 
+
+
   # 这是一个假接口 来对接口进行规范 xx:
   def example2
     render json: {
@@ -26,16 +28,55 @@ class Interface::UsersController < ActionController::Base
 
 
 
+  # 重点！！！！！
+  # 这是一个接口 来清晰我们几个概念例如
+  # 比如 1.each  2.map  3.{}  4.do end
+  #  do end 和 {} 是等价的 代码块 里面多用|| 滑竿
+  #  each 和 map 是两个方法，each返回单个数据，map返回数组集合
+  def example3
+     1. map 方法使用
+     names = User.all.map {|user| user[:name]}
+     render :json => names
+
+     2. map 方法使用
+     names = User.all.map {|user| user.name}
+     render :json => names
+
+     3. map 方法使用
+     names =User.all.map do |user|
+       user[:name]
+     end
+     render :json => names
+
+     4. 返回结果如下
+     [
+       "zhangyu",
+       "kjn",
+       "huangfeihong"
+     ]
+  end
+
+
   # 对数据库进行操作的接口,返回数据库中你想要的那些接口数据
   # 简单规范
   def all_users
-    users = User.all.map do |user|{
-        :id => user.id,
-        :name => user.name,
-        :sex => user.sex
-      }
-    end
+    users = User.all.map { |user| {
+        :id => user[:id],
+        :name => user[:name],
+        :sex => user[:sex]
+    }
+   }
+  render :json => users
+  end
 
+
+  def all_users2
+    users = User.all.map do |user| {
+      :id => user.id,
+      :name => user[:name],
+      :sex => user[:sex]
+    }
+    end
     render :json => users
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   #这个地方就是 创建controller就会有一个这样的引用文件
   resources :users
+
   #根路由
   root 'users#index'
 
@@ -13,6 +14,7 @@ Rails.application.routes.draw do
         #get :set_friends
         get :example
         get :all_users
+        get :all_users2
       end
     end
   end
